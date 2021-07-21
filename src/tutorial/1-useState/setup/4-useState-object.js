cdf63e9a4acc05a2534ba746ab1d6d463abe6a18
abhishek-parmar-au16 <abhishekparmar220197@gmail.com>
2021-07-21 16:58:05 +0530

    Done

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const[person,setPerson] = useState({name:'Abhishek',age:24,message:"Good Evening"});
+
+  return (
+        <>
+          <h2>{person.name}</h2>;
+        <h2>{person.age}</h2>;
+        <h2>{person.message}</h2>;
+        </>
+        );
 };
 
 export default UseStateObject;
