83608f081f79bd4890735a085cc8afc7709c7c44
abhishek-parmar-au16 <abhishekparmar220197@gmail.com>
2021-07-21 17:27:31 +0530

    Use operator

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -3,13 +3,23 @@ import React, { useState } from 'react';
 
 const UseStateObject = () => {
 
-  const[person,setPerson] = useState({name:'Abhishek',age:24,message:"Good Evening"});
+  // const[person,setPerson] = useState({name:'Abhishek',age:24,message:"Good Evening"});
+
+  const[name ,setName] = useState('loki');
+  const[age ,setAge] = useState(56);
+  const[message ,setMessage] = useState('wake up sid')
+
+  const onChange = ()=>{
+    // setPerson({...person,message:"hello dear"})
+    setMessage(" Do Exercise")
+  }
 
   return (
         <>
-          <h2>{person.name}</h2>;
-        <h2>{person.age}</h2>;
-        <h2>{person.message}</h2>;
+        <h2>{age}</h2>;
+        <h2>{name}</h2>;
+        <h2>{message}</h2>;
+        <button className="btn" onClick={onChange}>Change here</button>
         </>
         );
 };
